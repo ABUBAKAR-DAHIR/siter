2e62aba51a63abf5dc058d134e1d13728c3c1e0b
Abubakar dahir hassan <Abu112abu112abu112@gmail.com>
2026-06-26 17:14:31 +0530

    changing pm2 restart to pm2 reload for zero downtime

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <div>
-        <h2 className="font-semibold text-xl">Salaan qaaliya</h2>
+        <h2 className="text-lg">Salaan qaaliya</h2>
         <h1 className="font-bold text-2xl">Assalaamu calaykum waraxmatullaahi wabarakaatuh</h1>
     </div>
   )
